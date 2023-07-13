63df62d656fbdfe821faac2a4e89fb245d437b0f
mkreive <mkreive@gmail.com>
2023-07-13 14:34:36 +0300

    carousel redone

--- src/layouts/HomePage/components/ReturnBook.tsx
+++ src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
+import BookModel from '../../../models/BookModel';
 
-export const ReturnBook = () => {
+export const ReturnBook: React.FC<{ book: BookModel }> = ({ book }) => {
     return (
         <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
             <div className='text-center'>
-                <img
-                    src={require('./../../../Images/BooksImages/new-book-0.png')}
-                    width='151'
-                    height='233'
-                    alt='book'
-                />
-                <h6 className='mt-2'>The Song Of Achilles</h6>
-                <p>Madeline Miller</p>
+                {book.img ? (
+                    <img src={book.img} alt='book cover' width='151' height='233' />
+                ) : (
+                    <img
+                        src={require('./../../../Images/BooksImages/new-book-fallback.png')}
+                        width='151'
+                        height='233'
+                        alt='book cover'
+                    />
+                )}
+
+                <h6 className='mt-2'>{book.title}</h6>
+                <p>{book.author}</p>
                 <a className='btn secondary-color text-white' href='/books'>
                     Reserve
                 </a>
