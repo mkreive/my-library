a10ef752cd2ad4c97fb3ac85072d92b12d19ba90
mkreive <mkreive@gmail.com>
2023-07-17 10:08:43 +0300

    img

--- src/layouts/HomePage/components/ReturnBook.tsx
+++ src/layouts/HomePage/components/ReturnBook.tsx
@@ -7,7 +7,7 @@ export const ReturnBook: React.FC<{ book: BookModel }> = ({ book }) => {
         <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
             <div className='text-center'>
                 {book.img ? (
-                    <img src={book.img} alt='book cover' width='151' height='233' />
+                    <img src={`data:image/jpeg;base64,${book.img}`} alt='book cover' width='151' height='233' />
                 ) : (
                     <img
                         src={require('./../../../Images/BooksImages/new-book-fallback.png')}
