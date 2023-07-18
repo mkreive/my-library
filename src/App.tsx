10b99bad1c5106dae2b8f472043943a683c28338
mkreive <mkreive@gmail.com>
2023-07-18 10:18:35 +0300

    search book functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 // import { HomePage } from './layouts/HomePage/HomePage';
-import { SearBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
     return (
         <>
             <Navbar />
             {/* <HomePage /> */}
-            <SearBooksPage />
+            <SearchBooksPage />
             <Footer />
         </>
     );
--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import BookModel from '../../models/BookModel';
 import { SpinnerLoading } from '../Utils/SpinnerLoading';
 import { SearchBook } from './components/SearchBook';
 import { Pagination } from '../Utils/Pagination';
 
-export const SearBooksPage = () => {
+export const SearchBooksPage = () => {
     const [books, setBooks] = useState<BookModel[]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState(null);
@@ -12,21 +12,35 @@ export const SearBooksPage = () => {
     const [booksPerPage, setBooksPerPage] = useState(5);
     const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
     const [totalPages, setTotalPages] = useState(0);
+    const [search, setSearch] = useState('');
+    const [searchUrl, setSearchUrl] = useState('');
 
     useEffect(() => {
         const fetchBooks = async () => {
             const baseUrl: string = 'http://localhost:8080/api/books';
-            const url: string = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
+
+            let url: string = '';
+
+            if (searchUrl === '') {
+                url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
+            } else {
+                url = baseUrl + searchUrl;
+            }
+
             const response = await fetch(url);
+
             if (!response.ok) {
                 throw new Error('Something went wrong!');
             }
+
             const responseJson = await response.json();
+
             const responseData = responseJson._embedded.books;
-            const loadedBooks: BookModel[] = [];
 
             setTotalAmountOfBooks(responseJson.page.totalElements);
             setTotalPages(responseJson.page.totalPages);
+
+            const loadedBooks: BookModel[] = [];
 
             for (const key in responseData) {
                 loadedBooks.push({
@@ -44,13 +58,12 @@ export const SearBooksPage = () => {
             setBooks(loadedBooks);
             setIsLoading(false);
         };
-
         fetchBooks().catch((error: any) => {
             setIsLoading(false);
             setHttpError(error.message);
         });
         window.scrollTo(0, 0);
-    }, [currentPage]);
+    }, [currentPage, searchUrl, booksPerPage]);
 
     if (isLoading) {
         return <SpinnerLoading />;
@@ -63,6 +76,14 @@ export const SearBooksPage = () => {
             </div>
         );
     }
+
+    const searchHandleChange = () => {
+        if (search === '') {
+            setSearchUrl('');
+        } else {
+            setSearchUrl(`/search/findByTitleContaining?title=${search}&page=0&size=${booksPerPage}`);
+        }
+    };
 
     const indexOfLastBook: number = currentPage * booksPerPage;
     const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
@@ -81,8 +102,11 @@ export const SearBooksPage = () => {
                                     type='search'
                                     placeholder='Search'
                                     aria-labelledby='Search'
+                                    onChange={(e) => setSearch(e.target.value)}
                                 />
-                                <button className='btn btn-outline-success'>Search</button>
+                                <button className='btn btn-outline-success' onClick={() => searchHandleChange()}>
+                                    Search
+                                </button>
                             </div>
                         </div>
                         <div className='col-4'>
@@ -102,22 +126,22 @@ export const SearBooksPage = () => {
                                     </li>
                                     <li>
                                         <a className='dropdown-item' href='/home'>
-                                            Thriller
+                                            Front End
                                         </a>
                                     </li>
                                     <li>
                                         <a className='dropdown-item' href='/home'>
-                                            Romance
+                                            Back End
                                         </a>
                                     </li>
                                     <li>
                                         <a className='dropdown-item' href='/home'>
-                                            Comedy
+                                            Data
                                         </a>
                                     </li>
                                     <li>
                                         <a className='dropdown-item' href='/home'>
-                                            Biography
+                                            DevOps
                                         </a>
                                     </li>
                                 </ul>
@@ -125,15 +149,30 @@ export const SearBooksPage = () => {
                         </div>
                     </div>
 
-                    <div className='mt-3'>
-                        <h5>Number of results: {totalAmountOfBooks}</h5>
-                    </div>
-                    <p>
-                        {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
-                    </p>
-                    {books.map((book) => (
-                        <SearchBook book={book} key={book.id} />
-                    ))}
+                    {totalAmountOfBooks > 0 ? (
+                        <>
+                            <div className='mt-3'>
+                                <h5>Number of results: ({totalAmountOfBooks})</h5>
+                            </div>
+                            <p>
+                                {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items:
+                            </p>
+                            {books.map((book) => (
+                                <SearchBook book={book} key={book.id} />
+                            ))}
+                        </>
+                    ) : (
+                        <div className='m-5'>
+                            <h3>Can't find what you are looking for?</h3>
+                            <a
+                                type='button'
+                                className='btn main-color btn-md px-4 me-md-2 fw-bold text-white'
+                                href='/home'
+                            >
+                                Library Services
+                            </a>
+                        </div>
+                    )}
 
                     {totalPages > 1 && (
                         <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
