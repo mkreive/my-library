918199ac54a77f3ddebf288bbaab9221f5bf606f
mkreive <mkreive@gmail.com>
2023-07-21 11:11:20 +0300

    managelibrary admin stuff

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
+import { ManageLibraryPage } from './layouts/ManageLibraryPage/ManageLibraryPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -53,15 +54,19 @@ export const App = () => {
                             <BookCheckoutPage />
                         </Route>
 
-                        <Route path='/messages'>
-                            <MessagesPage />
-                        </Route>
-
                         <Route path='/login' render={() => <LoginWidget config={oktaConfig} />} />
                         <Route path='/login/callback' component={LoginCallback} />
 
                         <SecureRoute path='/shelf'>
                             <ShelfPage />
+                        </SecureRoute>
+
+                        <SecureRoute path='/messages'>
+                            <MessagesPage />
+                        </SecureRoute>
+
+                        <SecureRoute path='/admin'>
+                            <ManageLibraryPage />
                         </SecureRoute>
                     </Switch>
                 </div>
